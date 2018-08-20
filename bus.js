1a3bf8299ef028c3ad5a4cba3742b309e74f1b65
Carter Trafton <carter@qryde.local>
2018-08-20 10:49:51 -0400

    combining javascript files to simplify data

--- bus.js
+++ bus.js
@@ -5,17 +5,28 @@ var mymap = L.map('mapid').setView([42.630029,-71.353681], 13);
 var busIcon = L.icon({
     iconUrl: 'bus-icon.png',
 
-    iconSize:     [50, 50], // size of the icon
+    iconSize:     [40, 40], // size of the icon
     iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
-    popupAnchor:  [25, 0] // point from which the popup should open relative to the iconAnchor
+    popupAnchor:  [20, 20] // point from which the popup should open relative to the iconAnchor
 });
 
-// Temp data for bus routes
-// Each route has a color (for changing when selecting) and a route of GPS locations
+// Hard coded data for bus routes
 var data = [
-{   "ID": 'bus0',
-    "color": 'gray',
-    "route":
+{   'ID': 'V0000',
+    'segment' : 'S1111',
+    'start_time' :  '1111A',
+    'end_time' : '1111P',
+    'last_announce' : '1111A',
+    'battery_level' : '11%',
+    'next_stop' : '1111A-ID_NUMBER-STOP_NAME',
+    'total_stop' : '111',
+    'stop_announced' : '11',
+    'accuracy_percentage' : '11',
+    'device_time_onOff' : '#/##/2018 1111A',
+    'battery_levels_onOff' :  '11%',
+    'segment_time_onOff' : '1111A',
+    'color': 'gray',
+    'route':
         [[42.62978, -71.35376],
         [42.62949, -71.34976],
         [42.63178, -71.34461],
@@ -35,9 +46,22 @@ var data = [
         [42.63083, -71.35512],
         [42.62974, -71.35503]]
 }, // bus0
-{   "ID": 'bus1',
-    "color": 'gray',
-    "route":
+
+{   'ID': 'V1111',
+    'segment' : 'S2222',
+    'start_time' :  '2222A',
+    'end_time' : '2222P',
+    'last_announce' : '2222A',
+    'battery_level' : '22%',
+    'next_stop' : '2222A-ID_NUMBER-STOP_NAME',
+    'total_stop' : '222',
+    'stop_announced' : '22',
+    'accuracy_percentage' : '22',
+    'device_time_onOff' : '#/##/2018 2222A',
+    'battery_levels_onOff' :  '22%',
+    'segment_time_onOff' : '2222A',
+    'color': 'gray',
+    'route':
         [[42.6301252,	-71.3486881],
         [42.6284438,	-71.3480659],
         [42.6285938,	-71.3472183],
@@ -54,9 +78,22 @@ var data = [
         [42.6303857,	-71.3480873],
         [42.6334088,	-71.3491387]]
 }, //bus1
-{   "ID" : 'bus2',
-    "color": 'gray',
-    "route":
+
+{   'ID' : 'V2222',
+    'segment' : 'S3333',
+    'start_time' :  '3333A',
+    'end_time' : '3333P',
+    'last_announce' : '3333A',
+    'battery_level' : '33%',
+    'next_stop' : '3333A-ID_NUMBER-STOP_NAME',
+    'total_stop' : '333',
+    'stop_announced' : '33',
+    'accuracy_percentage' : '33',
+    'device_time_onOff' : '#/##/2018 3333A',
+    'battery_levels_onOff' :  '33%',
+    'segment_time_onOff' : '3333A',
+    'color': 'gray',
+    'route':
         [[42.6295578,	-71.3537991],
         [42.6297315,	-71.3550222],
         [42.6269213,	-71.3577902],
@@ -75,9 +112,9 @@ var data = [
 // Leaflet pre-written code for setting up map controls (zoom, scroll, etc)
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery � <a href="https://www.mapbox.com/">Mapbox</a>',
+	attribution: 'Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, ' +
+		'<a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, ' +
+		'Imagery � <a href='https://www.mapbox.com/'>Mapbox</a>',
 	id: 'mapbox.streets'
 }).addTo(mymap);
 
@@ -108,7 +145,7 @@ for (let i = 0; i < data.length; i++) {
     makeIcon(data[i]);
 }
 
-
+//Change colors of routes on map clicking
 mymap.on('click', function () {
     var tempColor = Math.floor(Math.random() * 3);
     var tempRoute = Math.floor(Math.random() * 3);
